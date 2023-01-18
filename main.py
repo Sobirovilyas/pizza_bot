5b72af0fa2ea3cbd69d195309b74ecc258a01ca8
GrayWizard12345 <muslim4ik14@gmail.com>
2023-01-18 11:41:57 +0500

    1. Added new data database 2. Products now are fetched from db 3. User is now saved into db

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from telebot import TeleBot
 from telebot.types import KeyboardButton, ReplyKeyboardMarkup
 import sqlite3
-from constants import get_products_query
-
+from constants import get_products_query, create_new_user_query
 
 TOKEN = '5943242364:AAEDa7ko4pgcCKnzSOw7WdvU8eYMH8OWD6M'
 
@@ -32,13 +31,16 @@ def get_product_names() -> list:
     try:
         conn = sqlite3.connect("pizza_database.db")
         cursor = conn.cursor()
+        sql = get_products_query()
+        cursor.execute(sql)
 
-
+        for product in cursor.fetchall():
+            products.append(product[0])
 
     except Exception as e:
         print(e)
 
-
+    return products
 def menu_keyboard():
     """
     Это меню высвечивает товары в нашей базе данных
@@ -65,10 +67,39 @@ def menu_keyboard():
     return keyboard
 
 
+def get_user_details_keyboard():
+
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    get_phone_button = KeyboardButton("Ввести номер телефона")
+    get_address_button = KeyboardButton("Ввести адрес")
+
+    keyboard.add(get_phone_button)
+    keyboard.add(get_address_button)
+
+    return keyboard
+
+
+def create_user(chat_id):
+
+    try:
+        conn = sqlite3.connect('pizza_database.db')
+        cursor = conn.cursor()
+        sql = create_new_user_query(chat_id)
+        cursor.execute(sql)
+
+    except Exception as e:
+        print(e)
+
+
+
 @bot.message_handler(commands=['start'])
 def start_handler(message):
+    chat_id = message.chat.id
+
+    create_user(chat_id)
+
     reply = "Вас приветствует бот доставки пиццы."
-    bot.reply_to(message, reply, reply_markup=main_menu_keyboard())
+    bot.reply_to(message, reply, reply_markup=get_user_details_keyboard())
 
 
 @bot.message_handler(func=lambda message: message.text == 'Меню')
